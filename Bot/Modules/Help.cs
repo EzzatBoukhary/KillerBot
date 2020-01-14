7d627c7662a9b4dd9034874f6a8e0cd1cb21b520
Ezzat Boukhary <Ezzat_eagle@hotmail.com>
2020-01-14 23:29:48 +0200

    fixed help bug ( V. 1.9.0)

--- Bot/Modules/Help.cs
+++ Bot/Modules/Help.cs
@@ -9,6 +9,7 @@ using Bot.Common;
 using Bot.Entities;
 using Bot.Handlers;
 using Bot.Helpers;
+using Bot.Preconditions;
 
 namespace Bot.Modules
 {
@@ -83,7 +84,7 @@ namespace Bot.Modules
 			}
 		}
 
-		[Command("help")]
+        /*[Command("help")]
 		[Alias("h", "command", "chelp", "ch")]
 		[Summary("Gets help on a specific command")]
 		public async Task HelpSpecific([Remainder] string query)
@@ -103,9 +104,109 @@ namespace Bot.Modules
 			if (embed.Fields.Count == 0)
 				embed.WithDescription("Nothing was found for " + query);
             await Context.Channel.SendMessageAsync("", false, embed.Build());
-		}
+		} */
+        [Command("help"), Alias("h")]
+        [Remarks("Shows what a specific command or module does and what parameters it takes.")]
+        [Cooldown(5)]
+        public async Task HelpQuery([Remainder] string query)
+        {
+            var builder = new EmbedBuilder();
 
-		private string FormatAliases(CommandInfo commandInfo)
+            builder.Color = new Color(241, 196, 15);
+            builder.Title = $"Help for '{query}'";
+            builder.WithFooter($"Requested by {Context.User}", Context.User.GetAvatarUrl());
+            builder.WithCurrentTimestamp();
+
+            var result = _cmdService.Search(Context, query);
+            if (query.StartsWith("module "))
+                query = query.Remove(0, "module ".Length);
+            var emb = result.IsSuccess ? HelpCommand(result, builder) : await HelpModule(query, builder);
+
+            if (emb.Fields.Length == 0)
+            {
+                await ReplyAsync($"Sorry, I couldn't find anything for \"{query}\".");
+                return;
+            }
+
+            await Context.Channel.SendMessageAsync("", false, emb);
+        }
+
+        private static Embed HelpCommand(SearchResult search, EmbedBuilder builder)
+        {
+            foreach (var match in search.Commands)
+            {
+                var cmd = match.Command;
+                var parameters = cmd.Parameters.Select(p => string.IsNullOrEmpty(p.Summary) ? p.Name : p.Summary);
+                var paramsString = $"Parameters: {string.Join(", ", parameters)}" +
+                                   //  $"\nPreconditions: {cmd.Preconditions.Humanize()}" +
+                                   (string.IsNullOrEmpty(cmd.Remarks) ? "" : $"\nRemarks: {cmd.Remarks}") +
+                                   (string.IsNullOrEmpty(cmd.Summary) ? "" : $"\nSummary: {cmd.Summary}");
+                ExampleAttribute example = cmd.Attributes.OfType<ExampleAttribute>().FirstOrDefault();
+                if (example != null && !example.ExampleText.IsEmpty())
+                {
+                    paramsString += $"\nExample: {example.ExampleText}";
+                }
+
+                builder.AddField(x =>
+                {
+                    x.Name = string.Join(", ", cmd.Aliases);
+                    x.Value = paramsString;
+                    x.IsInline = false;
+                });
+            }
+
+            return builder.Build();
+        }
+
+        private async Task<Embed> HelpModule(string moduleName, EmbedBuilder builder)
+        {
+            var module = _cmdService.Modules.ToList().Find(mod =>
+                string.Equals(mod.Name, moduleName, StringComparison.CurrentCultureIgnoreCase));
+            await AddModuleEmbedField(module, builder);
+            return builder.Build();
+        }
+
+        private async Task AddModuleEmbedField(ModuleInfo module, EmbedBuilder builder)
+        {
+            if (module is null) return;
+            var descriptionBuilder = new List<string>();
+            var duplicateChecker = new List<string>();
+            foreach (var cmd in module.Commands)
+            {
+                var result = await cmd.CheckPreconditionsAsync(Context);
+                if (!result.IsSuccess || duplicateChecker.Contains(cmd.Aliases.First())) continue;
+                duplicateChecker.Add(cmd.Aliases.First());
+                var cmdDescription = $"`{cmd.Aliases.First()}`";
+                if (!string.IsNullOrEmpty(cmd.Summary))
+                    cmdDescription += $" | {cmd.Summary}";
+                if (!string.IsNullOrEmpty(cmd.Remarks))
+                    cmdDescription += $" | {cmd.Remarks}";
+                if (cmdDescription != "``")
+                    descriptionBuilder.Add(cmdDescription);
+            }
+
+            if (descriptionBuilder.Count <= 0) return;
+            var builtString = string.Join("\n", descriptionBuilder);
+            var testLength = builtString.Length;
+            if (testLength >= 1024)
+            {
+                throw new ArgumentException("Value cannot exceed 1024 characters, please do `k!report help command value limit exceeded limit` if you see this message!");
+            }
+            var moduleNotes = "";
+            if (!string.IsNullOrEmpty(module.Summary))
+                moduleNotes += $" {module.Summary}";
+            if (!string.IsNullOrEmpty(module.Remarks))
+                moduleNotes += $" {module.Remarks}";
+            if (!string.IsNullOrEmpty(moduleNotes))
+                moduleNotes += "\n";
+            if (!string.IsNullOrEmpty(module.Name))
+            {
+                builder.AddField($"__**{module.Name}:**__",
+                    $"{moduleNotes} {builtString}\n{Constants.InvisibleString}");
+            }
+        }
+
+        private string FormatAliases(CommandInfo commandInfo)
 		{
 			IReadOnlyList<string> aliases = commandInfo.Aliases;
 
